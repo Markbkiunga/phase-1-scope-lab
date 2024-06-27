86096c6d3d1163a300b899f873499526b1787397
Mark Brian <markbkiunga2017@gmail.com>
2024-06-27 19:47:07 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+// console.log(customerName);
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  //   console.log(customerName);
+}
+// upperCaseCustomerName();
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  //   console.log(bestCustomer);
+}
+// setBestCustomer();
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  //   console.log(bestCustomer);
+}
+// overwriteBestCustomer();
+const leastFavoriteCustomer = "some initial value";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "some new value";
+}
+// changeLeastFavoriteCustomer();
+// console.log(leastFavoriteCustomer);
